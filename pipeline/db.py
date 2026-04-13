26cd658ad498175e9286ca3c28c8ee1e75bf28b6
Yubin Joe <yubis@Yubins-MacBook-Pro.local>
2026-04-13 01:23:13 -0700

    feat: SQLAlchemy engine factories for MySQL and PostgreSQL

---
+++ pipeline/db.py
@@ -0,0 +1,31 @@
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_mysql_engine():
+    url = (
+        "mysql+pymysql://{user}:{password}@{host}:{port}/{db}".format(
+            user=os.environ["MYSQL_USER"],
+            password=os.environ["MYSQL_PASSWORD"],
+            host=os.environ["MYSQL_HOST"],
+            port=os.environ.get("MYSQL_PORT", "3306"),
+            db=os.environ["MYSQL_DB"],
+        )
+    )
+    return create_engine(url)
+
+
+def get_postgres_engine():
+    url = (
+        "postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}".format(
+            user=os.environ["POSTGRES_USER"],
+            password=os.environ["POSTGRES_PASSWORD"],
+            host=os.environ["POSTGRES_HOST"],
+            port=os.environ.get("POSTGRES_PORT", "5432"),
+            db=os.environ["POSTGRES_DB"],
+        )
+    )
+    return create_engine(url)
